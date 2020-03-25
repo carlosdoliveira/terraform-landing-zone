651f94a6e1d3a438a59b5cf10fb0348c7e1c5cf7
Oliveira <carlos.oliveira@softwareone.com>
2020-03-25 17:13:12 -0300

    format

--- variables.tf
+++ variables.tf
@@ -1,34 +1,34 @@
 variable location {
-    type = string
-    default = "eastus"
-    description = "Definição da localidade onde será feito do deployment dos Recursos"
+  type        = string
+  default     = "eastus"
+  description = "Definição da localidade onde será feito do deployment dos Recursos"
 }
 
-variable "profile"{
-    type = string
-    default = "production"
+variable "profile" {
+  type    = string
+  default = "production"
 }
 
 variable "prefix" {
-    type = string
-    default = "lz-"
+  type    = string
+  default = "lz-"
 }
 
 variable "tags" {
-    type = map
-    default = {
-        Environment = "test"
-        Owner = "Carlos Oliveira"
-        Email = "carlos.oliveira@softwareone.com"
-    }
+  type = map
+  default = {
+    Environment = "test"
+    Owner       = "Carlos Oliveira"
+    Email       = "carlos.oliveira@softwareone.com"
+  }
 }
 
 variable "adds_name" {
-    type = string
-    default = "adds"
+  type    = string
+  default = "adds"
 }
 
 variable "adds_instance_count" {
-    type = number
-    default = 2
+  type    = number
+  default = 2
 }
