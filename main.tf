595392767f5d20b805e5c795cb7d0efbd7137432
Oliveira <carlos.oliveira@softwareone.com>
2020-03-25 17:53:12 -0300

    formatado para o terraform

--- main.tf
+++ main.tf
@@ -7,194 +7,194 @@ provider "azurerm" {
 
 # Resource Group #
 resource "azurerm_resource_group" "rg" {
-    name = "${var.prefix}rg-landingzone"
-    location = var.location
-    tags = var.tags
+  name     = "${var.prefix}rg-landingzone"
+  location = var.location
+  tags     = var.tags
 
-    lifecycle {
-        create_before_destroy = true
-    }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 # Networking Stuff
 
 ## Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-    name = "${var.prefix}vnet-hub"
-    address_space = ["172.16.0.0/16"]
-    resource_group_name = azurerm_resource_group.rg.name
-    location = var.location
-    tags = var.tags
-    dns_servers = ["172.16.0.4", "172.16.0.5"]
-    
+  name                = "${var.prefix}vnet-hub"
+  address_space       = ["172.16.0.0/16"]
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  tags                = var.tags
+  dns_servers         = ["172.16.0.4", "172.16.0.5"]
+
 }
 
 resource "azurerm_virtual_network" "spoke1" {
-    name = "${var.prefix}vnet-spoke1"
-    address_space = ["172.17.0.0/16"]
-    resource_group_name = azurerm_resource_group.rg.name
-    location = var.location
-    tags = var.tags
+  name                = "${var.prefix}vnet-spoke1"
+  address_space       = ["172.17.0.0/16"]
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  tags                = var.tags
 }
 resource "azurerm_virtual_network" "spoke2" {
-    name = "${var.prefix}vnet-spoke2"
-    address_space = ["172.18.0.0/16"]
-    resource_group_name = azurerm_resource_group.rg.name
-    location = var.location
-    tags = var.tags
+  name                = "${var.prefix}vnet-spoke2"
+  address_space       = ["172.18.0.0/16"]
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  tags                = var.tags
 }
 
 ## Subnets
 resource "azurerm_subnet" "identity" {
-    name = "Identity"
-    address_prefix = "172.16.0.0/24"
-    virtual_network_name = azurerm_virtual_network.vnet.name
-    resource_group_name = azurerm_resource_group.rg.name
+  name                 = "Identity"
+  address_prefix       = "172.16.0.0/24"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "management" {
-    name = "management"
-    address_prefix = "172.16.1.0/24"
-    virtual_network_name = azurerm_virtual_network.vnet.name
-    resource_group_name = azurerm_resource_group.rg.name
+  name                 = "management"
+  address_prefix       = "172.16.1.0/24"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "app" {
-    name = "frontend"
-    address_prefix = "172.17.0.0/24"
-    virtual_network_name = azurerm_virtual_network.spoke1.name
-    resource_group_name = azurerm_resource_group.rg.name
+  name                 = "frontend"
+  address_prefix       = "172.17.0.0/24"
+  virtual_network_name = azurerm_virtual_network.spoke1.name
+  resource_group_name  = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "sql" {
-    name = "backend"
-    address_prefix = "172.17.1.0/24"
-    virtual_network_name = azurerm_virtual_network.spoke1.name
-    resource_group_name = azurerm_resource_group.rg.name
+  name                 = "backend"
+  address_prefix       = "172.17.1.0/24"
+  virtual_network_name = azurerm_virtual_network.spoke1.name
+  resource_group_name  = azurerm_resource_group.rg.name
 }
 
 # Vnet Peerings
 
 resource "azurerm_virtual_network_peering" "hub-spoke1" {
-    name = "hub-spoke1"
-    resource_group_name = azurerm_resource_group.rg.name
-    virtual_network_name = azurerm_virtual_network.vnet.name
-    remote_virtual_network_id = azurerm_virtual_network.spoke1.id
+  name                      = "hub-spoke1"
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.vnet.name
+  remote_virtual_network_id = azurerm_virtual_network.spoke1.id
 }
 
 resource "azurerm_virtual_network_peering" "spoke1-hub" {
-    name = "spoke1-hub"
-    resource_group_name = azurerm_resource_group.rg.name
-    virtual_network_name = azurerm_virtual_network.spoke1.name
-    remote_virtual_network_id = azurerm_virtual_network.vnet.id
-    allow_forwarded_traffic = false
+  name                      = "spoke1-hub"
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.spoke1.name
+  remote_virtual_network_id = azurerm_virtual_network.vnet.id
+  allow_forwarded_traffic   = false
 }
 
 resource "azurerm_virtual_network_peering" "hub-spoke2" {
-    name = "hub-spoke2"
-    resource_group_name = azurerm_resource_group.rg.name
-    virtual_network_name = azurerm_virtual_network.vnet.name
-    remote_virtual_network_id = azurerm_virtual_network.spoke2.id
+  name                      = "hub-spoke2"
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.vnet.name
+  remote_virtual_network_id = azurerm_virtual_network.spoke2.id
 }
 
 resource "azurerm_virtual_network_peering" "spoke2-hub" {
-    name = "spoke2-hub"
-    resource_group_name = azurerm_resource_group.rg.name
-    virtual_network_name = azurerm_virtual_network.spoke2.name
-    remote_virtual_network_id = azurerm_virtual_network.vnet.id
-    allow_forwarded_traffic = false
+  name                      = "spoke2-hub"
+  resource_group_name       = azurerm_resource_group.rg.name
+  virtual_network_name      = azurerm_virtual_network.spoke2.name
+  remote_virtual_network_id = azurerm_virtual_network.vnet.id
+  allow_forwarded_traffic   = false
 }
 
 ## NSGs and rules ##
 resource "azurerm_network_security_group" "identity" {
-    name = "${var.prefix}nsg-identity"
-    location = var.location
-    resource_group_name = azurerm_resource_group.rg.name
-    tags = var.tags
+  name                = "${var.prefix}nsg-identity"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
 }
 
 resource "azurerm_network_security_group" "management" {
-    name = "${var.prefix}nsg-management"
-    location = var.location
-    resource_group_name = azurerm_resource_group.rg.name
-    tags = var.tags
+  name                = "${var.prefix}nsg-management"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
 }
 
 resource "azurerm_network_security_group" "app" {
-    name = "${var.prefix}nsg-app"
-    location = var.location
-    resource_group_name = azurerm_resource_group.rg.name
-    tags = var.tags
+  name                = "${var.prefix}nsg-app"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
 }
 
 resource "azurerm_network_security_group" "sql" {
-    name = "${var.prefix}nsg-SQL"
-    location = var.location
-    resource_group_name = azurerm_resource_group.rg.name
-    tags = var.tags
+  name                = "${var.prefix}nsg-SQL"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
 }
 
 resource "azurerm_network_security_rule" "RDP" {
-    name = "Allow_RDP_in"
-    direction = "inbound"
-    priority = 100
-    protocol = "*"
-    access = "allow"
-    source_port_range = "*"
-    destination_port_range = "3389"
-    source_address_prefix = "192.168.0.100"
-    destination_address_prefix = "*"
-    network_security_group_name = azurerm_network_security_group.management.name
-    resource_group_name = azurerm_resource_group.rg.name
+  name                        = "Allow_RDP_in"
+  direction                   = "inbound"
+  priority                    = 100
+  protocol                    = "*"
+  access                      = "allow"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "192.168.0.100"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.management.name
+  resource_group_name         = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_network_security_rule" "HTTTP-HTTPS" {
-    name = "Allow_HTTP-HTTPS_in"
-    direction = "inbound"
-    priority = 100
-    protocol = "*"
-    access = "allow"
-    source_port_range = "*"
-    destination_port_ranges = ["80","443"]
-    source_address_prefix = "*"
-    destination_address_prefix = "*"
-    network_security_group_name = azurerm_network_security_group.app.name
-    resource_group_name = azurerm_resource_group.rg.name
+  name                        = "Allow_HTTP-HTTPS_in"
+  direction                   = "inbound"
+  priority                    = 100
+  protocol                    = "*"
+  access                      = "allow"
+  source_port_range           = "*"
+  destination_port_ranges     = ["80", "443"]
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.app.name
+  resource_group_name         = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_network_security_rule" "sql" {
-    name = "Allow_SQL_in"
-    direction = "inbound"
-    priority = 100
-    protocol = "*"
-    access = "allow"
-    source_port_range = "*"
-    destination_port_range = "3306"
-    source_address_prefix = azurerm_subnet.app.address_prefix
-    destination_address_prefix = "*"
-    network_security_group_name = azurerm_network_security_group.sql.name
-    resource_group_name = azurerm_resource_group.rg.name
+  name                        = "Allow_SQL_in"
+  direction                   = "inbound"
+  priority                    = 100
+  protocol                    = "*"
+  access                      = "allow"
+  source_port_range           = "*"
+  destination_port_range      = "3306"
+  source_address_prefix       = azurerm_subnet.app.address_prefix
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.sql.name
+  resource_group_name         = azurerm_resource_group.rg.name
 }
 
 ## NSG Association
 resource "azurerm_subnet_network_security_group_association" "identity" {
-    network_security_group_id = azurerm_network_security_group.identity.id
-    subnet_id = azurerm_subnet.identity.id
+  network_security_group_id = azurerm_network_security_group.identity.id
+  subnet_id                 = azurerm_subnet.identity.id
 }
 
 resource "azurerm_subnet_network_security_group_association" "management" {
-    network_security_group_id = azurerm_network_security_group.management.id
-    subnet_id = azurerm_subnet.management.id
+  network_security_group_id = azurerm_network_security_group.management.id
+  subnet_id                 = azurerm_subnet.management.id
 }
 
 resource "azurerm_subnet_network_security_group_association" "app" {
-    network_security_group_id = azurerm_network_security_group.app.id
-    subnet_id = azurerm_subnet.app.id
+  network_security_group_id = azurerm_network_security_group.app.id
+  subnet_id                 = azurerm_subnet.app.id
 }
 
 resource "azurerm_subnet_network_security_group_association" "sql" {
-    network_security_group_id = azurerm_network_security_group.sql.id
-    subnet_id = azurerm_subnet.sql.id
+  network_security_group_id = azurerm_network_security_group.sql.id
+  subnet_id                 = azurerm_subnet.sql.id
 }
 
 # Compute Resources
@@ -202,26 +202,26 @@ resource "azurerm_subnet_network_security_group_association" "sql" {
 ## Availability Set
 
 resource "azurerm_availability_set" "adds" {
-    name                         = "${var.prefix}adds-avset"
-    location                     = azurerm_resource_group.rg.location
-    resource_group_name          = azurerm_resource_group.rg.name
-    platform_fault_domain_count  = 2
-    platform_update_domain_count = 2
-    managed                      = true
+  name                         = "${var.prefix}adds-avset"
+  location                     = azurerm_resource_group.rg.location
+  resource_group_name          = azurerm_resource_group.rg.name
+  platform_fault_domain_count  = 2
+  platform_update_domain_count = 2
+  managed                      = true
 }
 
 resource "azurerm_network_interface" "adds" {
 
-    name                = "${var.prefix}${var.adds_name}-nic${count.index}"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    count = var.adds_instance_count
+  name                = "${var.prefix}${var.adds_name}-nic${count.index}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  count               = var.adds_instance_count
 
-    ip_configuration {
-        name                          = "ipconfig1"
-        subnet_id                     = azurerm_subnet.identity.id
-        private_ip_address_allocation = "Dynamic"
-    }
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.identity.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
 
 resource "azurerm_virtual_machine" "adds" {
@@ -230,7 +230,7 @@ resource "azurerm_virtual_machine" "adds" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [element(azurerm_network_interface.adds.*.id, count.index)]
-  availability_set_id = azurerm_availability_set.adds.id
+  availability_set_id   = azurerm_availability_set.adds.id
   vm_size               = "Standard_B2ms"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
@@ -257,9 +257,9 @@ resource "azurerm_virtual_machine" "adds" {
     admin_password = "#POC@dmin2020"
   }
   os_profile_windows_config {
-        timezone = "E. South America Standard Time"
-        provision_vm_agent = true
-        enable_automatic_upgrades = true
+    timezone                  = "E. South America Standard Time"
+    provision_vm_agent        = true
+    enable_automatic_upgrades = true
 
   }
   tags = {
